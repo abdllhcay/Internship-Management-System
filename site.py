99c2425c403820d02dabe08ab948aba0b5fa59fc
Abdullah <abdllhcay@gmail.com>
2018-10-13 10:12:18 +0300

    Initial commit

---
+++ site.py
@@ -0,0 +1,13 @@
+# -*- coding:utf8 -*-
+
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
